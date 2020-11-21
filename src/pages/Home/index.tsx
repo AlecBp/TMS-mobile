c2d4ddeca600470900f9699d425d432611c32985
Vincent-hide <hidehiro.okayama@gmail.com>
2020-11-21 18:48:55 -0500

    past sessions added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,6 @@ import SessionCard from "../../components/SessionCard"
 
 // @ts-ignore
 function Home({navigation}) {
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.spaceAround}>
@@ -22,12 +21,13 @@ function Home({navigation}) {
         <Avatar.Text size={60} label="XD"/>
       </View>
 
-      <View style={[styles.spaceAround, {marginTop: 10}]}>
+      <View style={[styles.spaceAround, {marginVertical: 20}]}>
         <Button mode="outlined" onPress={() => navigation.navigate("PastSessions")}>See Past Sessions</Button>
         <Button mode="contained">Logout</Button>
       </View>
 
       <FlatList
+        // keyExtractor={(item) => item.id}
         data={sessions}
         renderItem={({item}) => {
           return <SessionCard session={item}/>
