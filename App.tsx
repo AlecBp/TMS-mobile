85449c3d25f38352e8b307b5979b8472e8fb312d
Alec <alec.paglia@gmail.com>
2020-11-22 00:13:45 -0500

    Removed unused imports

--- App.tsx
+++ App.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import Apollo from "./src/components/Apollo";
 import Root from "./src/components/Root";
 
-import Footer from "./src/components/Footer";
-import LoginPage from "./src/pages/LoginPage";
-
-
 // @ts-ignore
 import { UserProvider } from "./src/context/UserContext";
-import Routes from "./src/components/Routes"
 
 export default function App() {
   return (
