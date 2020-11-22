8848885ebf265f00b10dae75c0076f43c0f1db96
Rafael Silva <rafonsosilva@gmail.com>
2020-11-21 21:16:23 -0500

    Add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, } from 'react-native';
+import { Text } from "react-native-paper";
+
+
+const styles = StyleSheet.create({
+    
+})
+
+const Footer = () => {
+
+    return (
+        <>
+        <Text>Tutoring Management System © 2020 - V 0.1.0</Text>
+        </>
+    )
+}
