387781f5f6c08d414b32a44c9eec296e7068db27
Alec <alec.paglia@gmail.com>
2020-11-22 18:30:38 -0500

    Main version of the app.tsx

--- App.tsx
+++ App.tsx
@@ -2,31 +2,15 @@ import React from "react";
 import Apollo from "./src/components/Apollo";
 import Root from "./src/components/Root";
 
-import LoginPage from "./src/pages/LoginPage";
-import SessionDetails from "./src/pages/SessionDetails";
-
-
-// @ts-ignore
-import { UserProvider } from "./src/context/UserContext";
-import Routes from "./src/components/Routes"
-import Home from "./src/pages/Home";
-import PastSessions from "./src/pages/PastSessions";
-import TutorPage from "./src/pages/TutorPage";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
 // @ts-ignore
 import { UserProvider } from "./src/context/UserContext";
 
 export default function App() {
   return (
-
     <UserProvider>
       <Apollo>
         <Root />
       </Apollo>
     </UserProvider>
-
   );
 }
