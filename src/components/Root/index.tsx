3d3e2f6cea5c390e082b31ceb0724f85b2d07a03
Alec <alec.paglia@gmail.com>
2020-11-22 21:11:16 -0500

    Root now using env var

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,43 +1,41 @@
-import React, {useContext, useEffect, useState} from "react";
-import {View, Text, ActivityIndicator} from "react-native";
-import {setAccessToken} from "../../auth/accessToken";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import { setAccessToken } from "../../auth/accessToken";
 import Routes from "../Routes";
 // @ts-ignore
-import {UserContext, SET_ACCESS_TOKEN, CLEAR} from "./../../context/UserContext";
+import { UserContext, SET_ACCESS_TOKEN, CLEAR } from "./../../context/UserContext";
 import LoadingSpinner from "../LoadingSpinner";
+import getEnvVars from "../../../env";
 
-
-const BASE_URL: string = "http://192.168.0.160:4000";
+// const BASE_URL: string = "http://192.168.0.160:4000";
 
 const Root: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
-  const {dispatch} = useContext(UserContext);
+  const { dispatch } = useContext(UserContext);
 
   useEffect(() => {
-    fetch(`${BASE_URL}/refresh_token`, {
+    fetch(getEnvVars().refreshTokenUrl, {
       method: "POST",
       credentials: "include",
     }).then(async (x) => {
-      const {ok, accessToken} = await x.json();
+      const { ok, accessToken } = await x.json();
       if (ok) {
         setAccessToken(accessToken);
-        dispatch({type: SET_ACCESS_TOKEN, payload: {accessToken}});
+        dispatch({ type: SET_ACCESS_TOKEN, payload: { accessToken } });
       } else {
         setAccessToken("");
-        dispatch({type: CLEAR});
+        dispatch({ type: CLEAR });
       }
       setLoading(false);
     });
   }, []);
 
   if (loading) {
-    return (
-      <LoadingSpinner text='Loading' size='large' color='#0000ff' />
-    );
+    return <LoadingSpinner text="Loading" size="large" color="#0000ff" />;
   }
 
-  return <Routes/>;
+  return <Routes />;
 };
 
 export default Root;
